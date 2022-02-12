89de21b8394bf2eb4bc534de09798ddb3a29819e
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-02-11 19:13:38 -0500

    set up vite to transpile typescript

--- server.cjs
+++ server.cjs
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => console.log(`server launched on port ${port}`));
+app.listen(port, () => console.log(`server is running http://localhost:${port}/`));
