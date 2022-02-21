8f7b2735037211106e63b7386ee110ada6ead14a
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-02-21 14:29:19 -0500

    add info page

--- server.cjs
+++ server.cjs
@@ -1,13 +1,25 @@
 const express = require('express');
+const DeviceDetector = require('node-device-detector');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+const detector = new DeviceDetector;
 
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index');
+    const userAgent = req.get('User-Agent');
+    const result = detector.detect(userAgent);
+
+    let clientName = result.client.name;
+    let deviceType = result.device.type;
+
+    if (clientName === "Chrome" && deviceType === "desktop") {
+        res.sendFile(__dirname + '/index.html');
+    }
+
+    res.sendFile(__dirname + '/info.html');
 });
 
 app.listen(port, () => console.log(`server is running http://localhost:${port}/`));
